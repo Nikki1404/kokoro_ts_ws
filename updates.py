6b062e0c957ba637621f75c84365578757f31c65
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-02-23 22:19:47 +0530

    Update updates.py

--- updates.py
+++ updates.py
@@ -7,3 +7,6 @@ docker build \
   --build-arg http_proxy=http://163.116.128.80:8080 \
   --build-arg https_proxy=http://163.116.128.80:8080 \
   -t kokoro_ws .
+
+gcloud config set project emr-dgt-autonomous-uctr1-snbx
+gcloud auth configure-docker us-central1-docker.pkg.dev
