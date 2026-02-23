430f9756b17d5bbab88c2b9aa46128afe5ac30be
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-02-23 21:07:27 +0530

    Update updates.py

--- updates.py
+++ updates.py
@@ -2,3 +2,6 @@ us-central1-docker.pkg.dev/emr-dgt-autonomous-uctr1-snbx/cx-speech
 now i need to push the docker image in this path via cloud run 
 docker build us-central1-docker.pkg.dev/emr-dgt-autonomous-uctr1-snbx/cx-speech/kokor_ws:1.0.0 .
 docker build us-central1-docker.pkg.dev/emr-dgt-autonomous-uctr1-snbx/cx-speech/kokor_openai:1.0.0 .
+
+gcloud config set project emr-dgt-autonomous-uctr1-snbx
+gcloud auth configure-docker us-central1-docker.pkg.dev
