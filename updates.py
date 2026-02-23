b569e3e48437e4e463a18ce08008aaa500fff56a
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-02-23 17:46:47 +0530

    Update updates.py

--- updates.py
+++ updates.py
@@ -1,4 +1,28 @@
-try:
-    from kokoro import KPipeline
-except ImportError:
-    from kokoro_tts import KPipeline
+FROM nvidia/cuda:12.1.1-runtime-ubuntu22.04
+
+ENV https_proxy="http://163.116.128.80:8080"
+ENV http_proxy="http://163.116.128.80:8080"
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+WORKDIR /app
+
+RUN apt-get update && apt-get install -y \
+    git ffmpeg python3 python3-pip && \
+    rm -rf /var/lib/apt/lists/*
+
+ENV PYTHONDONTWRITEBYTECODE=1 \
+    PYTHONUNBUFFERED=1 \
+    KOKORO_LANG=a \
+    KOKORO_DEFAULT_VOICE=af_heart \
+    KOKORO_PRELOAD_VOICES="af_heart af_bella af_sky"
+
+COPY . /app/
+
+RUN  pip install --no-cache-dir -r /app/requirements.txt
+
+RUN pip install onnxruntime-gpu
+
+EXPOSE 8080
+
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8080"]
