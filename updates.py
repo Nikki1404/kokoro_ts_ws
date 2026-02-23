a6b65833b0f98f0bb2e57277762d75b9abbf2112
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-02-23 21:20:45 +0530

    Update updates.py

--- updates.py
+++ updates.py
@@ -7,3 +7,6 @@ gcloud config set project emr-dgt-autonomous-uctr1-snbx
 gcloud auth configure-docker us-central1-docker.pkg.dev
 docker tag kokoro_openai_tts:latest \
 us-central1-docker.pkg.dev/emr-dgt-autonomous-uctr1-snbx/cx-speech/kokoro_openai:1.0.0
+
+docker tag kokoro_ws:latest \
+us-central1-docker.pkg.dev/emr-dgt-autonomous-uctr1-snbx/cx-speech/kokoro_ws:1.0.0
