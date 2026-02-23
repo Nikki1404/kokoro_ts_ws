17f7ea0a67e35e66f7bd4542eb365160a27b208c
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-02-23 18:12:39 +0530

    Update updates.py

--- updates.py
+++ updates.py
@@ -1,48 +1,3 @@
-#requirements.txt-
-fastapi
-kokoro-tts
-numpy
-openai
-onnxruntime-gpu
-pydantic-settings
-pydub
-requests
-sounddevice
-uvicorn
-
-#Dockerifle-
-FROM nvidia/cuda:12.1.1-runtime-ubuntu22.04
-
-ENV https_proxy="http://163.116.128.80:8080"
-ENV http_proxy="http://163.116.128.80:8080"
-
-ENV DEBIAN_FRONTEND=noninteractive
-
-WORKDIR /app
-
-RUN apt-get update && apt-get install -y \
-    git ffmpeg python3 python3-pip \
-    portaudio19-dev libportaudio2 libasound2-dev \
-    && rm -rf /var/lib/apt/lists/*
-
-ENV PYTHONDONTWRITEBYTECODE=1 \
-    PYTHONUNBUFFERED=1 \
-    KOKORO_LANG=a \
-    KOKORO_DEFAULT_VOICE=af_heart \
-    KOKORO_PRELOAD_VOICES="af_heart af_bella af_sky"
-
-COPY . /app/
-
-RUN  pip install --no-cache-dir -r /app/requirements.txt
-
-RUN pip install onnxruntime-gpu
-
-EXPOSE 8080
-
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8080"]
-
-I am getting this 
-
 (base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/cx-speech-tts/fastapi_impl_gpu# docker run --gpus all -p 8080:8080 -e KOKORO_API_KEY=devkey kokoro_openai_tts
 
 ==========
@@ -59,6 +14,7 @@ https://developer.nvidia.com/ngc/nvidia-deep-learning-container-license
 
 A copy of this license is made available in this container at /NGC-DL-CONTAINER-LICENSE for your convenience.
 
+Consider using the pymupdf_layout package for a greatly improved page layout analysis.
 Traceback (most recent call last):
   File "/app/app/tts/kokoro_engine.py", line 10, in <module>
     from kokoro import KPipeline
@@ -109,8 +65,4 @@ Traceback (most recent call last):
     from app.tts.kokoro_engine import synthesize_np, encode_audio, maybe_save
   File "/app/app/tts/kokoro_engine.py", line 12, in <module>
     from kokoro_tts import KPipeline
-  File "/usr/local/lib/python3.10/dist-packages/kokoro_tts/__init__.py", line 20, in <module>
-    import sounddevice as sd
-  File "/usr/local/lib/python3.10/dist-packages/sounddevice.py", line 72, in <module>
-    raise OSError('PortAudio library not found')
-OSError: PortAudio library not found
+ImportError: cannot import name 'KPipeline' from 'kokoro_tts' (/usr/local/lib/python3.10/dist-packages/kokoro_tts/__init__.py)
