d5d15537da416ddb9cb252918f300e037ca5a51a
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-02-23 18:52:28 +0530

    Update updates.py

--- updates.py
+++ updates.py
@@ -1,10 +1,10 @@
-(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/cx-speech-tts/fastapi_impl_gpu# docker run --gpus all -p 8080:8080 -e KOKORO_API_KEY=devkey kokoro_openai_tts
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/cx-speech-chatterbox# docker logs 45b8f22480a0
 
 ==========
 == CUDA ==
 ==========
 
-CUDA Version 12.1.1
+CUDA Version 12.1.0
 
 Container image Copyright (c) 2016-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
@@ -14,55 +14,28 @@ https://developer.nvidia.com/ngc/nvidia-deep-learning-container-license
 
 A copy of this license is made available in this container at /NGC-DL-CONTAINER-LICENSE for your convenience.
 
-Consider using the pymupdf_layout package for a greatly improved page layout analysis.
-Traceback (most recent call last):
-  File "/app/app/tts/kokoro_engine.py", line 10, in <module>
-    from kokoro import KPipeline
-ModuleNotFoundError: No module named 'kokoro'
+WARNING: The NVIDIA Driver was not detected.  GPU functionality will not be available.
+   Use the NVIDIA Container Toolkit to start this container with GPU support; see
+   https://docs.nvidia.com/datacenter/cloud-native/ .
 
-During handling of the above exception, another exception occurred:
+*************************
+** DEPRECATION NOTICE! **
+*************************
+THIS IMAGE IS DEPRECATED and is scheduled for DELETION.
+    https://gitlab.com/nvidia/container-images/cuda/blob/master/doc/support-policy.md
 
-Traceback (most recent call last):
-  File "/usr/local/bin/uvicorn", line 8, in <module>
-    sys.exit(main())
-  File "/usr/local/lib/python3.10/dist-packages/click/core.py", line 1485, in __call__
-    return self.main(*args, **kwargs)
-  File "/usr/local/lib/python3.10/dist-packages/click/core.py", line 1406, in main
-    rv = self.invoke(ctx)
-  File "/usr/local/lib/python3.10/dist-packages/click/core.py", line 1269, in invoke
-    return ctx.invoke(self.callback, **ctx.params)
-  File "/usr/local/lib/python3.10/dist-packages/click/core.py", line 824, in invoke
-    return callback(*args, **kwargs)
-  File "/usr/local/lib/python3.10/dist-packages/uvicorn/main.py", line 433, in main
-    run(
-  File "/usr/local/lib/python3.10/dist-packages/uvicorn/main.py", line 606, in run
-    server.run()
-  File "/usr/local/lib/python3.10/dist-packages/uvicorn/server.py", line 75, in run
-    return asyncio_run(self.serve(sockets=sockets), loop_factory=self.config.get_loop_factory())
-  File "/usr/local/lib/python3.10/dist-packages/uvicorn/_compat.py", line 60, in asyncio_run
-    return loop.run_until_complete(main)
-  File "/usr/lib/python3.10/asyncio/base_events.py", line 649, in run_until_complete
-    return future.result()
-  File "/usr/local/lib/python3.10/dist-packages/uvicorn/server.py", line 79, in serve
-    await self._serve(sockets)
-  File "/usr/local/lib/python3.10/dist-packages/uvicorn/server.py", line 86, in _serve
-    config.load()
-  File "/usr/local/lib/python3.10/dist-packages/uvicorn/config.py", line 441, in load
-    self.loaded_app = import_from_string(self.app)
-  File "/usr/local/lib/python3.10/dist-packages/uvicorn/importer.py", line 19, in import_from_string
-    module = importlib.import_module(module_str)
-  File "/usr/lib/python3.10/importlib/__init__.py", line 126, in import_module
-    return _bootstrap._gcd_import(name[level:], package, level)
-  File "<frozen importlib._bootstrap>", line 1050, in _gcd_import
-  File "<frozen importlib._bootstrap>", line 1027, in _find_and_load
-  File "<frozen importlib._bootstrap>", line 1006, in _find_and_load_unlocked
-  File "<frozen importlib._bootstrap>", line 688, in _load_unlocked
-  File "<frozen importlib._bootstrap_external>", line 883, in exec_module
-  File "<frozen importlib._bootstrap>", line 241, in _call_with_frames_removed
-  File "/app/app/main.py", line 4, in <module>
-    from app.routers.openai_compatible import router as openai_router
-  File "/app/app/routers/openai_compatible.py", line 8, in <module>
-    from app.tts.kokoro_engine import synthesize_np, encode_audio, maybe_save
-  File "/app/app/tts/kokoro_engine.py", line 12, in <module>
-    from kokoro_tts import KPipeline
-ImportError: cannot import name 'KPipeline' from 'kokoro_tts' (/usr/local/lib/python3.10/dist-packages/kokoro_tts/__init__.py)
+/usr/local/lib/python3.10/dist-packages/diffusers/models/lora.py:393: FutureWarning: `LoRACompatibleLinear` is deprecated and will be removed in version 1.0.0. Use of `LoRACompatibleLinear` is deprecated. Please switch to PEFT backend by installing PEFT: `pip install peft`.
+  deprecate("LoRACompatibleLinear", "1.0.0", deprecation_message)
+INFO:root:input frame rate=25
+/usr/lib/python3.10/contextlib.py:103: FutureWarning: `torch.backends.cuda.sdp_kernel()` is deprecated. In the future, this context manager will be removed. Please see `torch.nn.attention.sdpa_kernel()` for the new context manager, with updated signature.
+  self.gen = func(*args, **kwds)
+LlamaModel is using LlamaSdpaAttention, but `torch.nn.functional.scaled_dot_product_attention` does not support `output_attentions=True`. Falling back to the manual attention implementation, but specifying the manual implementation will be required from Transformers version v5.0.0 onwards. This warning can be removed using the argument `attn_implementation="eager"` when loading the model.
+Loading Chatterbox on device: cpu
+loaded PerthNet (Implicit) at step 250,000
+Sampling:   0%|          | 0/1000 [00:00<?, ?it/s]We detected that you are passing `past_key_values` as a tuple of tuples. This is deprecated and will be removed in v4.47. Please convert your cache or use an appropriate `Cache` class (https://huggingface.co/docs/transformers/kv_cache#legacy-cache-format)
+Sampling:   2%|▏         | 15/1000 [00:01<02:02,  8.01it/s]INFO:chatterbox.models.t3.t3:✅ EOS token detected! Stopping generation at step 16
+Sampling:   2%|▏         | 15/1000 [00:01<02:04,  7.89it/s]
+INFO:     Started server process [1]
+INFO:     Waiting for application startup.
+INFO:     Application startup complete.
+INFO:     Uvicorn running on http://0.0.0.0:8002 (Press CTRL+C to quit)
