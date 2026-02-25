a1fc249ea240a916963dc5e5de5361e6fa8d3059
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-02-25 14:38:10 +0530

    Update updates.py

--- updates.py
+++ updates.py
@@ -1,16 +1,14 @@
-RUN python3 -m pip install --upgrade pip
-
-RUN python3 -m pip install --no-cache-dir \
-    "nemo_toolkit[asr] @ git+https://github.com/NVIDIA/NeMo.git@main"
-
-RUN python3 -m pip install --no-cache-dir --force-reinstall \
-    --index-url https://download.pytorch.org/whl/cu124 \
-    torch==2.5.1 \
-    torchaudio==2.5.1
-
-COPY requirements-server.txt .
-RUN python3 -m pip install --no-cache-dir -r requirements-server.txt
-
-RUN python3 -c "import torch, torchaudio; torch.hub.load('snakers4/silero-vad','silero_vad',trust_repo=True); print('Silero cached')"
-
-COPY server.py .
+ => CACHED [ 6/10] RUN python3 -m pip install --no-cache-dir --force-reinstall     --index-url https://download.pytorch.org/whl/cu124     torch==2.5.  0.0s
+ => ERROR [ 7/10] COPY requirements-server.txt .                                                                                                       0.0s
+------
+ > [ 7/10] COPY requirements-server.txt .:
+------
+Dockerfile:26
+--------------------
+  24 |         torchaudio==2.5.1
+  25 |
+  26 | >>> COPY requirements-server.txt .
+  27 |     RUN python3 -m pip install --no-cache-dir -r requirements-server.txt
+  28 |
+--------------------
+ERROR: failed to build: failed to solve: failed to compute cache key: failed to calculate checksum of ref df342f6d-5d3e-4045-80a0-9a044ee86345::gbblpyfyq8rml11u43p1j7p37: "/requirements-server.txt": not found
